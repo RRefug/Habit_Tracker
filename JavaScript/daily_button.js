18fcf68c21e84a32de0e9a4f986a0e6c63b8b033
RRefug <99227044+RRefug@users.noreply.github.com>
2026-01-15 17:22:27 -0800

    runGreeting.js is more of a test file, but use for verifying javascript functions from daily_button work. Also, daily.html has a working button now.

---
+++ JavaScript/daily_button.js
@@ -0,0 +1,13 @@
+
+
+
+function showGreeting(){
+    let greeting = 'Hello World!'
+
+    console.log(greeting) // Inspect page and you will see message if you click the check in button on thw website! :D
+}
+
+
+// Optional: Step 1 of running functions in your terminal. If you want to be extra ;}
+// Rest of info is in runGreeting.js file. Also, don't forget to cd into JavaScript folder before doing this :)
+module.exports = { showGreeting }
---
+++ JavaScript/runGreeting.js
@@ -0,0 +1,14 @@
+
+
+// In case you want to run functions in your terminal. Just for testing! :)
+// Also, don't forget to cd into JavaScript folder before doing this :)
+// Step one. You have to export your functions from the initial file: daily_button.js
+// Using module.exports = { showGreeting }
+// Step two. You go to the file you want to see the function output at: runGreeting
+// Using code below and bam! :D done!
+
+
+
+const {showGreeting } = require('./daily_button')
+
+showGreeting()

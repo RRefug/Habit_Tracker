1368b7b8381162c298eb570074a39f178cf1e1ec
RRefug <99227044+RRefug@users.noreply.github.com>
2026-02-10 17:12:16 -0800

    Able to save habits_obj to local storage.

--- JavaScript/habits.js
+++ JavaScript/habits.js
@@ -36,9 +36,28 @@ export class Habits {
             li.textContent = habit;
             habits_list.appendChild(li);
         });
+
+        // update the local storage habit's object.
+
+         //* 1. Push habits object to local storage - The array should be empty when starting 
+        let my_habits_serialized = JSON.stringify(habits_obj);
+        localStorage.setItem('string_habits_object', my_habits_serialized);
+
+        // // grab string from local storage
+        // const my_habits_local_storage_value = localStorage.getItem('string_habits_object');
+
+        // // parse string into a json object
+        // const my_habits_local_storage_Obj = JSON.parse(habits_obj);
+
+        // // push back to local storage, by stringifying
+        // let my_habits_serialized = JSON.stringify(my_habits_local_storage_Obj);
+        // localStorage.setItem('string_habits_object', my_habits_serialized);
+
+        console.log('UPDATED Local Storage...')
+
     }
 
-    // not being used, maybe delete. 
+    // not being used, maybe delete. It's being used in index.js. Line 32.
     view_habits(){
         let size = this.habits_list.length;
 
@@ -55,7 +74,26 @@ const habits_obj = new Habits();
 // Grab DOM elements (ids) from habits.html. 
 const habits_list = document.getElementById("habits_html_list");
 
+
+// when html page loads
 document.addEventListener("DOMContentLoaded", () => {
+
+    //* 1. Push habits object to local storage - The array should be empty when starting 
+    // let my_habits_serialized = JSON.stringify(habits_obj);
+    // localStorage.setItem('string_habits_object', my_habits_serialized);
+
+    // 2. When updating habits array on html page, with new update button, pull object from local storage
+
+    // 3. Parse the object
+
+    // 4. Update the changes and push it back to local storage
+
+    // 5. Refresh html page, or close browser, and see if there exists an object in local storage. If so, done! Will use that object to pass to index.html page.\
+
+    // 6. Refactor too so it's not super clumped here.
+
+    // *
+
     const btn = document.getElementById("submitHabitBtn");
     console.log("Button:", btn);
 
